34ed68c906122b2c3eab5cbc1632e63c76555017
GuiGui <gchafiol@vehizen.com>
2024-05-13 15:01:04 +0200

    versions ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pokerstatsAngular';
+    title = 'pokerstatsAngular';
+
+    constructor(){
+      const a = [1].at(0)
+    }
 }
