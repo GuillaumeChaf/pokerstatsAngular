258d1c095aa5489df919b98cb6a5575caed55e5a
GuiGui <gchafiol@vehizen.com>
2024-09-07 19:26:35 +0200

    finitions + lexiques

--- src/app/components/secondary-components/directions/directions.component.ts
+++ src/app/components/secondary-components/directions/directions.component.ts
@@ -1,12 +1,27 @@
+import { KeyValue, KeyValuePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { conditions } from 'src/app/models/conditions';
 
 @Component({
   selector: 'app-directions',
   standalone: true,
-  imports: [],
+  imports: [KeyValuePipe],
   templateUrl: './directions.component.html',
-  styleUrl: './directions.component.scss'
+  styleUrl: './directions.component.scss',
 })
 export class DirectionsComponent {
+  /** liste des conditions */
+  conditions = conditions;
+  /** exte expliquant le principe d'ajout et suppression d'un joueur dans le coup */
+  addDelLegend: string = 'Entrée/Sortie des joueurs dans le calcul de la main';
+  /** texte explqiuant le principe de la sélection de carte */
+  selectionCardLegend: string = 'Sélection des cartes, tous les joueurs dans le coup doivent avoir leur 2 cartes sélectionnées';
+  /** texte expliquant le principe de condition de calcul */
+  conditionLegend = 'Le pourcentage affiché sera la probabilité de ...';
+  /** texte expliquant le principe des combinaisons finale */
+  finalCombinationLegend: string = 'Probabilité octroyant en plus la <strong>CF</strong> au joueur';
 
+  orderFnc: (a: KeyValue<string, any>, b: KeyValue<string, any>) => number = (a, b) => {
+    return a.value.order - b.value.order;
+  };
 }
--- src/app/models/conditions.ts
+++ src/app/models/conditions.ts
@@ -1,9 +1,16 @@
 export type condition = 'win' | 'finish' | 'lose';
 
-export type conditionConfig = { value: string; color: string; viewBox: string; title: string; order: number };
+export type conditionConfig = { value: string; color: string; viewBox: string; title: string; order: number; legend: string };
 
 export const conditions: { [key in condition]: conditionConfig } = {
-  win: { value: 'win', color: '#2d972d', viewBox: '0 0 512 512', title: 'victoire', order: 0 },
-  finish: { value: 'finish', color: '#db8546', viewBox: '0 0 448 576', title: 'obtention', order: 1 },
-  lose: { value: 'lose', color: '#cf0000', viewBox: '0 0 448 512', title: 'défaite', order: 2 },
+  win: { value: 'win', color: '#2d972d', viewBox: '0 0 512 512', title: 'victoire', order: 0, legend: ' ... gagner avec la <strong>CF</strong>' },
+  finish: {
+    value: 'finish',
+    color: '#db8546',
+    viewBox: '0 0 448 576',
+    title: 'obtention',
+    order: 1,
+    legend: ' ... finir avec la <strong>CF</strong>',
+  },
+  lose: { value: 'lose', color: '#cf0000', viewBox: '0 0 448 512', title: 'défaite', order: 2, legend: ' ... perdre avec la <strong>CF</strong>' },
 };
